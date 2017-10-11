e233df014e18b98a0c6d4b2103b9c6e924c9b8cf
Redwoods Yi <chaos21c@gmail.com>
2017-10-11 13:21:41 +0900

    wk06 lec & src upload

---
+++ advanced-Arduino-iot/src/wk05_AAnn_src/AAnn_2Leds/AAnn_2Leds.ino
@@ -0,0 +1,23 @@
+/*
+ LED 교차 점멸
+*/
+
+const int ledA   =  3;
+const int ledB   =  5;
+
+void setup()
+{
+  pinMode(ledA, OUTPUT);
+  pinMode(ledB, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledA,HIGH);
+  digitalWrite(ledB,LOW);
+  delay(100);
+  digitalWrite(ledA,LOW);
+  digitalWrite(ledB,HIGH);
+  delay(100);
+}
+
