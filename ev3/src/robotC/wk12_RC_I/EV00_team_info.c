42cd285701034165691ca715061d58758da8e07c
Redwoods Yi <chaos21c@gmail.com>
2017-11-20 08:31:08 +0900

    wk13 lec upload

---
+++ ev3/src/robotC/wk12_RC_I/EV00_team_info.c
@@ -0,0 +1,22 @@
+//int z = 77;
+//float y = 2.373;
+
+task main()
+{
+	//char x = 'a';  // 'a' -> 97
+	//x = 98;
+	//
+	//
+	string s1 = "EV00";
+	string s2 = "Redwoods, Chaos";
+	int acNum = 77;
+	string s31 = "Stay hungry, ";
+	string s32 = "stay foolish.";
+
+	displayBigTextLine(0, "Show EV00 Info.");
+	displayBigTextLine(3, "Name: %s", s2);
+	displayBigTextLine(6, "acNUM: %d", acNum);
+	displayBigTextLine(9, s31);
+	sleep(5000);
+
+}
---
+++ ev3/src/robotC/wk12_RC_I/wk12_RC02float.c
@@ -0,0 +1,10 @@
+
+task main()
+{
+	float x;
+	x = 3.141592;
+
+	displayBigTextLine(5, "x is %f", x);
+	sleep(5000);
+
+}
